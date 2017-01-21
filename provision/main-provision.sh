89d9b8a48e0785844b5b40f749c031820b465aab
AndreasHerde <batman@herde.com>
2017-01-21 23:35:21 +0100

    initial commit

---
+++ provision/main-provision.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# Copyright 2017 Andreas Herde <batman@herde.com>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This file is maintained at http://github.com/AndreasHerde/vagrant-basics
+
+###########################################################
+# Edit Vagrantconf before provisioning.
+###########################################################
+
+# Import shared resources.
+source /vagrant/Vagrantconf
+
+###########################################################
+# No need to edit below this line.
+###########################################################
+
+# Make it non interactive installation.
+export DEBIAN_FRONTEND=noninteractive
+
+# Configure swap.
+${PROVISION_SH_DIR}/core-swap.sh 
+
+# Configure environment, install most important software.
+${PROVISION_SH_DIR}/core-core.sh
+
+# Provision enabled.
+
+if [ "${APACHE_ENABLE}" == "yes" ]; then
+    ${PROVISION_SH_DIR}/prov-apache.sh
+fi
+
+if [ "${NGINX_ENABLE}" == "yes" ]; then
+    ${PROVISION_SH_DIR}/prov-nginx.sh
+fi
+
+if [ "${PHP5_ENABLE}" == "yes" ] && [ "${PHP7_ENABLE}" == "yes" ]; then
+    echo "====================================================================="
+    echo "You must enable either PHP5 or PHP7 not both"
+    echo "After provisioning run one of the scripts:"
+    echo "${PROVISION_SH_DIR}/prov-php5.sh or ${PROVISION_SH_DIR}/prov-php7.sh "
+    echo "====================================================================="
+else
+    if [ "${PHP5_ENABLE}" == "yes" ]; then
+        ${PROVISION_SH_DIR}/prov-php5.sh
+    fi
+
+    if [ "${PHP7_ENABLE}" == "yes" ]; then
+        ${PROVISION_SH_DIR}/prov-php7.sh
+    fi
+fi
+
+if [ "${MYSQL_ENABLE}" == "yes" ]; then
+    ${PROVISION_SH_DIR}/prov-mysql.sh
+fi
+
+if [ "${MEMCACHED_ENABLE}" == "yes" ]; then
+    ${PROVISION_SH_DIR}/prov-memcached.sh
+fi
+
+if [ "${REDIS_ENABLE}" == "yes" ]; then
+    ${PROVISION_SH_DIR}/prov-redis.sh
+fi
+
+if [ "${BEANSTALK_ENABLE}" == "yes" ]; then
+    ${PROVISION_SH_DIR}/prov-beanstalked.sh
+fi
